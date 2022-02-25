76d55b5d589980ee95da97ffc59819787183c560
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-02-25 16:00:59 +0000

    a tag added to download

--- src/components/profile/ProfileCardComponents/DropMore.jsx
+++ src/components/profile/ProfileCardComponents/DropMore.jsx
@@ -6,27 +6,27 @@ export default function  DropMore ({profile}) {
 
 
 
-    const downloadPDF = async(e) => {
-        try {
-        const response = await fetch(`${process.env.REACT_APP_PROD_URL}/profiles/${profile._id}/downloadPdf`)
-        if(response.ok){
-            window.location.replace(`${process.env.REACT_APP_PROD_URL}/profiles/${profile._id}/downloadPdf`);
-        }
-        } catch (error) {
-          console.log(error);
-        }
-      };
+    // const downloadPDF = async(e) => {
+    //     try {
+    //     const response = await fetch(`${process.env.REACT_APP_PROD_URL}/profiles/${profile._id}/downloadPdf`)
+    //     if(response.ok){
+    //         window.location.replace(`${process.env.REACT_APP_PROD_URL}/profiles/${profile._id}/downloadPdf`);
+    //     }
+    //     } catch (error) {
+    //       console.log(error);
+    //     }
+    //   };
 
-      const downloadCSV = async(e) => {
-        try {
-            const response = await fetch(`${process.env.REACT_APP_PROD_URL}/profiles/${profile._id}/experiences/csv`)
-            if(response.ok){    
-            window.location.replace(`${process.env.REACT_APP_PROD_URL}/profiles/${profile._id}/experiences/csv`);
-            }
-        } catch (error) {
-          console.log(error);
-        }
-      };
+    //   const downloadCSV = async(e) => {
+    //     try {
+    //         const response = await fetch(`${process.env.REACT_APP_PROD_URL}/profiles/${profile._id}/experiences/csv`)
+    //         if(response.ok){    
+    //         window.location.replace(`${process.env.REACT_APP_PROD_URL}/profiles/${profile._id}/experiences/csv`);
+    //         }
+    //     } catch (error) {
+    //       console.log(error);
+    //     }
+    //   };
 
     return (<>
          <ul className='list-unstyled'>
@@ -36,11 +36,15 @@ export default function  DropMore ({profile}) {
                             </li>
                                 <li >
                                     <span className="mx-3"><FiDownload/></span>
-                                  <span onClick={(e) => downloadPDF()}>Save to pdf</span> 
+                                    <a  href={`${process.env.REACT_APP_PROD_URL}/profiles/${profile._id}/downloadPdf`}>
+                                    <span >Save to pdf</span> 
+                                    </a>
                                 </li>
                                 <li >
                                     <span className="mx-3"><GrDocument/></span>
-                                 <span className="text-left" onClick={(e) => downloadCSV()}>Build a resume</span>
+                                    <a href={`${process.env.REACT_APP_PROD_URL}/profiles/${profile._id}/experiences/csv`}>
+                                    <span className="text-left" >Build a resume</span>
+                                    </a>
                                 </li>
                             </ul>
     </>
